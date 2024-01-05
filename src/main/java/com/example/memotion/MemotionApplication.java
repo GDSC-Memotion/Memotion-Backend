01277aef64045a5c7bedf6d8b2269b97c63e9829
Han Ju Kim <hjkim2902@gmail.com>
2024-01-05 15:57:45 +0900

    init project

---
+++ src/main/java/com/example/memotion/MemotionApplication.java
@@ -0,0 +1,13 @@
+package com.example.memotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemotionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memotion/MemotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
