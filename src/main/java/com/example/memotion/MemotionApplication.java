faa474f2c58db9a8f6b0944224db12f2a9eec2a4
Han Ju Kim <hjkim2902@gmail.com>
2024-01-08 14:40:30 +0900

    feat : 테스트용 데이터베이스 소스 자동 연결 제거

--- src/main/java/com/example/memotion/MemotionApplication.java
+++ src/main/java/com/example/memotion/MemotionApplication.java
@@ -2,8 +2,9 @@ package com.example.memotion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MemotionApplication {
 
     public static void main(String[] args) {
