9bcfe427af1076571e0f594344c8d1f6b5ee5e64
Han Ju Kim <hjkim2902@gmail.com>
2024-01-22 02:12:01 +0900

    fix : 일기 작성 시 여러 이미지 받도록 수정

--- src/main/java/com/example/memotion/dto/CreateRecordReq.java
+++ src/main/java/com/example/memotion/dto/CreateRecordReq.java
@@ -3,10 +3,12 @@ package com.example.memotion.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class CreateRecordReq {
     private String description;
     private String mood;
-    private String imageUri;
+    private List<String> imageUris;
 }
--- src/main/java/com/example/memotion/service/RecordService.java
+++ src/main/java/com/example/memotion/service/RecordService.java
@@ -17,17 +17,28 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class RecordService {
     private final RecordRepository recordRepository;
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
+    private final ImageRepository imageRepository;
+    private final RecordImageRepository recordImageRepository;
 
     public Long addRecord(CreateRecordReq createRecordReq) {
-        Member member = userRepository.findById(1L).orElseThrow(() -> new NoSuchElementException());
+        Member member = memberRepository.findById(1L).orElseThrow(() -> new NoSuchElementException());
         Record record = Record.builder()
-                .imageUrl(createRecordReq.getImageUri())
                 .mood(MoodEnum.from(createRecordReq.getMood()))
                 .description(createRecordReq.getDescription())
                 .member(member)
                 .build();
+        List<String> imageUris = createRecordReq.getImageUris().stream()
+                .map(e -> e.toString()).toList();
 
+        List<Image> images = imageUris.stream()
+                .map(Image::new)
+                .toList();
+
+        imageRepository.saveAll(images);
+        for (Image image : images) {
+            recordImageRepository.save(new RecordImage(record, image));
+        }
         Record save = recordRepository.save(record);
         return save.getId();
     }
