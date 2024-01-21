c246e61234b7257a9c881f97b13a26aaa658cdec
Han Ju Kim <hjkim2902@gmail.com>
2024-01-22 00:04:30 +0900

    feat : 기본 바환 객체 생성

---
+++ src/main/java/com/example/memotion/common/BaseResponse.java
@@ -0,0 +1,45 @@
+package com.example.memotion.common;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+import static com.example.memotion.common.ResponseStatus.SUCCESS;
+
+@Getter
+@JsonPropertyOrder({"code", "status", "message", "timestamp", "result"})
+public class BaseResponse<T> {
+
+    private final int code;
+    private final int status;
+    private final String message;
+    private final LocalDateTime timestamp;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T result;
+
+    public BaseResponse(T result) {
+        this.code = SUCCESS.getCode();
+        this.status = SUCCESS.getStatus();
+        this.message = SUCCESS.getMessage();
+        this.timestamp = LocalDateTime.now();
+        this.result = result;
+    }
+    public BaseResponse(ResponseStatus status) {
+        this.code = status.getCode();
+        this.status = status.getStatus();
+        this.message = status.getMessage();
+        this.result = null;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public BaseResponse(ResponseStatus status, String message) {
+        this.code = status.getCode();
+        this.status = status.getStatus();
+        this.message = message;
+        this.result = null;
+        this.timestamp = LocalDateTime.now();
+    }
+}
