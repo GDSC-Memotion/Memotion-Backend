e42de6e503454b310b51556c83e2112929e740fb
Han Ju Kim <hjkim2902@gmail.com>
2024-02-07 18:57:28 +0900

    feat : Diary에 Status 추가

---
+++ src/main/java/com/example/memotion/reecord/domain/Diary.java
@@ -0,0 +1,56 @@
+package com.example.memotion.reecord.domain;
+
+import com.example.memotion.common.domain.BaseEntity;
+import com.example.memotion.common.domain.STATUS;
+import com.example.memotion.image.domain.Image;
+import com.example.memotion.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "diary")
+@ToString
+@NoArgsConstructor
+public class Diary extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "diary")
+    private List<Image> images;
+
+    @Enumerated(value = EnumType.STRING)
+    private STATUS status = STATUS.ACTIVATE;
+
+    @Builder
+    public Diary(Long id, String description, Member member) {
+        this.id = id;
+        this.description = description;
+        this.member = member;
+    }
+}
